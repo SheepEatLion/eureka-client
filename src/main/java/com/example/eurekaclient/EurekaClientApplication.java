37ae0a39d2c701db04d3d0d634ea22f9ecf98192
junyeong <gowjr207@gmail.com>
2022-05-25 14:49:30 +0900

    feat: eureka client setting

--- src/main/java/com/example/eurekaclient/EurekaClientApplication.java
+++ src/main/java/com/example/eurekaclient/EurekaClientApplication.java
@@ -2,7 +2,14 @@ package com.example.eurekaclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ *  eureka client
+ *
+ */
+
+@EnableEurekaClient
 @SpringBootApplication
 public class EurekaClientApplication {
 
